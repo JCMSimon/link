fd65b8f9beac1cd9f54775ef675e7aca5acd90a2
JustCallMeSimon <github@jcms.dev>
2024-02-24 21:14:20 +0100

    im stupid

--- redirect.js
+++ redirect.js
@@ -1,15 +1,11 @@
 document.addEventListener("DOMContentLoaded", function() {
     const destination = window.location.pathname.substring(1);
     console.log(destination)
-    const infoElem = document.getElementsByClassName("redirectText")[0]
+    const infoElem = document.getElementsByClassName("redirectingText")[0]
     if (destination == "") {
         // no redirect
-        infoElem.innerHTML = "No redirect found"
-        infoElem.innerText = "No redirect found"
         infoElem.textContent = "No redirect found"
     } else {
-            infoElem.innerHTML = `Going to ${destination}`
-            infoElem.innerText = `Going to ${destination}`
             infoElem.textContent = `Going to ${destination}`    
         }
     }
