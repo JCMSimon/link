eaa7b388a5280b44c57362695443335457651429
JustCallMeSimon <github@jcms.dev>
2024-02-24 21:12:58 +0100

    fdawd

--- redirect.js
+++ redirect.js
@@ -4,8 +4,12 @@ document.addEventListener("DOMContentLoaded", function() {
     const infoElem = document.getElementsByClassName("redirectText")[0]
     if (destination == "") {
         // no redirect
+        infoElem.innerHTML = "No redirect found"
+        infoElem.innerText = "No redirect found"
         infoElem.textContent = "No redirect found"
-        } else {
+    } else {
+            infoElem.innerHTML = `Going to ${destination}`
+            infoElem.innerText = `Going to ${destination}`
             infoElem.textContent = `Going to ${destination}`    
         }
     }
